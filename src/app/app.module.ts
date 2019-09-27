e7a1e618137e7690bf35e507c44336a94d914fd8
Himanshusachdev30 <Himanshusachdev30@gmail.com>
2019-09-27 08:05:00 -0400

    login component added

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AppMaterial } from './app.material';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HeaderComponent
+  ],
+  imports: [AppMaterial,
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
